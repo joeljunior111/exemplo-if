43ad3c29741051f5c94decff28359194b6efc213
Joel Junior Nunes Araujo <junioraraujojp111@gmail.com>
2022-09-12 19:30:28 -0300

    conhecendo o git primeiro commit

---
+++ function soma () {.js
@@ -0,0 +1,8 @@
+function soma () {
+    var n1 = document.getElementById("n1").valuer;
+    var n2 = document.getElementById("n2").valuer;
+    var resultado = document.getElementById("resultado");
+    var conta = parseInt(n1) + parseInt(n2);
+    document.innerHTML = conta;
+
+}
